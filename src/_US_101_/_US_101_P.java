d392079474ed4cfa193dff4f7b450a50ff9adb79
depom1 <ahmet.36.2000@hotmail.com>
2024-03-07 13:20:05 +0300

    _US_101_DONE

---
+++ src/_US_101_/_US_101_P.java
@@ -0,0 +1,72 @@
+package _US_101_;
+
+import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+public class _US_101_P extends BaseDriver {
+    @Test
+    public void test(){
+        driver.get("https://www.akakce.com/");
+        WebElement openAnAccount= driver.findElement(By.xpath("//div[@id=\"H_rl_v8\"]/a[1]"));
+        openAnAccount.click();
+
+        WebElement name= driver.findElement(By.xpath("//input[@id=\"rnufn\"]"));
+        name.sendKeys("John");
+
+        WebElement surname= driver.findElement(By.xpath("//input[@id=\"rnufs\"]"));
+        surname.sendKeys("Lion");
+
+        WebElement email= driver.findElement(By.xpath("//input[@id=\"rnufe1\"]"));
+        email.sendKeys("wipano6954@sfpixel.com");
+
+        WebElement emailAgain=driver.findElement(By.xpath("//input[@id=\"rnufe2\"]"));
+        emailAgain.sendKeys("wipano6954@sfpixel.com");
+
+        WebElement password=driver.findElement(By.xpath("//input[@id=\"rnufp1\"]"));
+        password.sendKeys("Logic.123456");
+
+        WebElement passwordRepeat=driver.findElement(By.xpath("//input[@id=\"rnufp2\"]"));
+        passwordRepeat.sendKeys("Logic.123456");
+
+        WebElement Gender=driver.findElement(By.xpath("//input[@id=\"rngm\"]"));
+        Gender.click();
+
+        WebElement city=driver.findElement(By.xpath("//select[@id=\"locpr\"]"));
+        Select secim=new Select(city);
+        secim.selectByIndex(2);
+
+        WebElement district=driver.findElement(By.xpath("//select[@id=\"locds\"]"));
+        Select secim2=new Select(district);
+        secim2.selectByIndex(14);
+
+        WebElement day=driver.findElement(By.xpath("//select[@id=\"bd\"]"));
+        Select secim3=new Select(day);
+        secim3.selectByIndex(13);
+
+        WebElement month=driver.findElement(By.xpath("//select[@id=\"bm\"]"));
+        Select secim4=new Select(month);
+        secim4.selectByIndex(10);
+
+        WebElement years=driver.findElement(By.xpath("//select[@id=\"by\"]"));
+        Select secim5=new Select(years);
+        secim5.selectByVisibleText("2000");
+
+        WebElement terms=driver.findElement(By.xpath("//input[@id=\"rnufpca\"]"));
+        terms.click();
+
+        WebElement campaigns=driver.findElement(By.xpath("//input[@id=\"rnufnee\"]"));
+        campaigns.click();
+
+        WebElement oAnAccountBtn=driver.findElement(By.xpath("//input[@id=\"rfb\"]"));
+        oAnAccountBtn.click();
+
+        WebElement dogrulama=driver.findElement(By.xpath("//*[text()='John']"));
+        wait.until(ExpectedConditions.textToBe(By.xpath("//*[text()='John']"),"John"));
+        Assert.assertTrue("Başarılı Giriş",dogrulama.getText().contains("John"));
+    }
+}
